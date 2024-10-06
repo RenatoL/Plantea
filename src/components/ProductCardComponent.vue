316015ec9a652a159c3e74e64f988395530021fe
Renato Lourinho <lourinho.renato@gmail.com>
2024-10-06 21:57:15 +0100

    feat: update product card component layout

--- src/components/ProductCardComponent.vue
+++ src/components/ProductCardComponent.vue
@@ -17,6 +17,7 @@ const props = defineProps([
   'quantity'
 ])
 const refName = props.refName
+const id = props.id
 
 const price = computed(() => {
   if (props.prices && props.prices.length > 0) {
@@ -46,7 +47,7 @@ const images = Object.fromEntries(
     <div @click="productView" class="before:link-cover">
       <div class="theme-tile rounded-2xl lg:rounded-4xl relative overflow-hidden h-full">
         <div class="p-2 xl:p-6 xl:py-2">
-          <div class="image overflow-hidden w-full aspect-square">
+          <div class="image overflow-hidden w-[75%] aspect-auto">
             <picture>
               <img :src="images[`${refName}`]" :alt="name" />
             </picture>
