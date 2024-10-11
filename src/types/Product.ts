1d010bcea933915c4ad312093f046604e2881b90
Renato Lourinho <lourinho.renato@gmail.com>
2024-10-11 13:52:08 +0100

    Added products assets

---
+++ src/types/Product.ts
@@ -0,0 +1,29 @@
+interface Product {
+    id: string
+    name: string
+    description: string
+    category: string
+    type: string
+    mainImage: string
+    refName: string
+    prices: ProductPrice[]
+  }
+
+  interface ProductPrice {
+    priceId: string
+    priceActive: boolean
+    priceCurrency: string
+    priceType: string
+    priceProductId: string
+    priceBillingScheme: string
+    priceDescription: string
+    priceUnitAmount: number
+    priceMetadata: PriceMetadata[]
+  }
+  
+  interface PriceMetadata {
+    [key: string]: boolean;
+  
+  }
+
+  export type { Product }
